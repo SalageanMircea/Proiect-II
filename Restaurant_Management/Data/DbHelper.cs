c65b4cbcad5cb8e2544f7c695b1594809515488f
RockLionIT <pakot.p0014@gmail.com>
2026-04-25 17:59:07 +0300

    Mutarea bazelor de date si eliminarea cailor relative din fisierul DBHelper.cs, adoptarea unei alte abordari

--- Restaurant_Management/Data/DbHelper.cs
+++ Restaurant_Management/Data/DbHelper.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.Data.SqlClient;
+using System;
+using System.IO;
+
 
 namespace Restaurant_Management.Data
 {
     public static class DbHelper
     {
-        private static string connectionString =
-            @"Data Source=np:\\.\pipe\LOCALDB#4E33D12D\tsql\query;Initial Catalog=Restaurant_DB_App;AttachDbFilename=D:\Faculta\An3_sem2\II\Proiect\Proiect-II\Restaurant_Management\Restaurant_DB.mdf;Integrated Security=True;Connect Timeout=30";
+        private static readonly string connectionString;
 
+        static DbHelper()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "..", "..", "..", "..", "..", "..", "Database")));
+
+            connectionString =
+                "Data Source=(LocalDB)\\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|\\Restaurant_DB.mdf;" +
+                "Integrated Security=True;TrustServerCertificate=True";
+        }
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(connectionString);
